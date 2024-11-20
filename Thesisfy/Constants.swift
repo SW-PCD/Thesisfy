f7aeda737b36748d43dcfc3a6cb6e699a48b771f
ctokkm <2271488.hansung.ac.kr>
2024-11-20 14:37:54 +0900

    [feat]로그인&회원가입&홈화면

---
+++ Thesisfy/Constants.swift
@@ -0,0 +1,45 @@
+//
+//  Constants.swift
+//  Thesisfy
+//
+//  Created by KKM on 10/20/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants {
+    // MARK: - Color
+    static let GrayColorWhite: Color = .white
+    static let GrayColorGray900: Color = Color(red: 0.1, green: 0.1, blue: 0.1)
+    static let GrayColorGray400: Color = Color(red: 0.71, green: 0.71, blue: 0.71)
+    static let PrimaryColorPrimary500: Color = Color(red: 0.18, green: 0.56, blue: 1)
+    static let PrimaryColorPrimary100: Color = Color(red: 0.74, green: 0.86, blue: 1)
+    static let GrayColorGray800: Color = Color(red: 0.23, green: 0.23, blue: 0.23)
+    static let GrayColorGray50: Color = Color(red: 0.98, green: 0.98, blue: 0.98)
+    static let GrayColorGray100: Color = Color(red: 0.95, green: 0.95, blue: 0.95)
+    static let SemanticColorNegativeNegative400: Color = Color(red: 1, green: 0.29, blue: 0.29)
+
+
+    
+    // MARK: - Font Size
+    static let fontSizeXxxs: CGFloat = 10
+    static let fontSizeXs: CGFloat = 12
+    static let fontSizeS: CGFloat = 14
+    static let fontSizeM: CGFloat = 16
+    static let fontSizeL: CGFloat = 18
+    static let fontSizeXl: CGFloat = 20
+    
+    // MARK: - Font Weight
+    static let fontWeightRegular: Font.Weight = .regular
+    static let fontWeightMedium: Font.Weight = .medium
+    static let fontWeightSemibold: Font.Weight = .semibold
+    static let fontWeightBold: Font.Weight = .bold
+
+    
+    // MARK: - Border Color
+    static let BorderColorBorder2: Color = .black.opacity(0.1)
+    static let BorderColorBorder1: Color = .black.opacity(0.05)
+    
+    
+}
