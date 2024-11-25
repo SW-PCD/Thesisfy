5c4a1335b7d5037369560d8b76ff4f82ff0dd4a9
feelow7 <ghkdvlfgh7@hansung.ac.kr>
2024-11-26 03:02:48 +0900

    feelow develop

---
+++ Thesisfy/ThesisfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThesisfyApp.swift
+//  Thesisfy
+//
+//  Created by 황필호 on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ThesisfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MyPageView()
+        }
+    }
+}
